f827d9995757c21cfdb537ad8c9e061c83bc399b
AnnaKurginyan <anna.kurginyan@student.rau.am>
2025-01-05 23:17:22 +0400

    исправление подключения к серверу

--- initialize_db.py
+++ initialize_db.py
@@ -1,39 +1,16 @@
 import psycopg2
 from psycopg2 import sql
 
+conn = psycopg2.connect(dbname="postgres", user="annaKURG", password="anna27", host="localhost", port="5432")
+conn.autocommit=True
+cursor = conn.cursor()
 
-def create_database(db_name, db_owner, user, password, host='172.17.0.2', port=5432):
+cursor.execute(sql.SQL("CREATE DATABASE {}").format(sql.Identifier("Shipping")))
+cursor.execute(sql.SQL("ALTER DATABASE {} OWNER TO {}").format(sql.Identifier("Shipping"), sql.Identifier(user)))
+cursor.close()
+conn.close()
 
-    conn = psycopg2.connect(
-        dbname='postgres',  
-        user=user,
-        password=password,
-        host=host,
-        port=port
-    )
-    
-    conn.autocommit = True  
-    cursor = conn.cursor()
-
-
-    create_database = sql.SQL("CREATE DATABASE {} WITH OWNER = {};").format(
-        sql.Identifier(db_name),  
-        sql.Identifier(db_owner)  
-    )
-    
-    try:
-        cursor.execute(create_database)
-        print(f"Created successfully. DB_NAME: {db_name}   DB_OWNER: {db_owner}")
-    except psycopg2.errors.DuplicateDatabase:
-        print(f"Database already exists")
-    
-
-    cursor.close()
-    conn.close()
-
-db_name = 'Shipping'  
-db_owner = 'annaKURG'  
-user = 'annaKURG'  
-password = 'anna27'  
-create_database(db_name, db_owner, user, password)
-
+# db_name = 'Shipping'  
+# db_owner = 'annaKURG'  
+# user = 'annaKURG'  
+# password = 'anna27'
